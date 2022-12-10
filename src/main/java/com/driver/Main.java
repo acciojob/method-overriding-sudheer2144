1619f5fa6ea2b8fdc224565ba7d687d13b285258
Sudheer <sudheer.ged@gmail.com>
2022-12-10 15:25:24 +0530

    Sudheer commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,14 +11,5 @@ public class Main {
   
 }
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
 
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extended class B";
-    }
-}
+
