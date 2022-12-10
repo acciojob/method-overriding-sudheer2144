2c0aac63eb8ea565b1ba61712b30feccb9049db0
Sudheer <sudheer.ged@gmail.com>
2022-12-10 15:42:07 +0530

    Sudheer commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,9 @@ package com.driver;
 public class Main {
 
     public static void main(String[] args) {
-        B bobj=new B();
-        A aobj=new A();
-        System.out.println(aobj.meth());
-        System.out.println(bobj.meth());
+        A obj=new B();
+        System.out.println(obj.meth());
+        //obj.meth();
     }
   
 }
