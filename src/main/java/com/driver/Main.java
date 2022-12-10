04048feb72d4180e5d64eb64d8dac05f6820cb0f
Sudheer <sudheer.ged@gmail.com>
2022-12-10 15:10:26 +0530

    Sudheer commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        B obj=new B();
+        System.out.println(obj.meth());
+    }
   
+}
+
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
 }
